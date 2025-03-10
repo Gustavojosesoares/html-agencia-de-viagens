305e78c8c6da26f065e57ffc24a0258fabcbdf41
Gustavojosesoares <gustavosoaresalvesdasilva@gmail.com>
2025-03-10 16:19:10 -0300

    adição de novos arquivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,11 +126,7 @@
 	// Nav Panel.
 
 		// Toggle.
-			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
-			)
-				.appendTo($wrapper);
-
+		
 			// Change toggle styling once we've scrolled past the header.
 				$header.scrollex({
 					bottom: '5vh',
@@ -143,6 +139,8 @@
 				});
 
 		// Panel.
+		
+
 			$navPanel = $(
 				'<div id="navPanel">' +
 					'<nav>' +
